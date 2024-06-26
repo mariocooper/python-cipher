f930656d993c44bfb63a383513bd26a863c0ca59
Mario Cooper <mariocooper00@hotmail.com>
2024-06-26 23:32:28 +0100

    added parameters to caesar() function

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 text = 'Hello Zaira'
 shift = 3
-def caesar():
+
+def caesar(message, offset):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     encrypted_text = ''
 
-    for char in text.lower():
+    for char in message.lower():
         if char == ' ':
             encrypted_text += char
         else:
             index = alphabet.find(char)
-            new_index = (index + shift) % len(alphabet)
+            new_index = (index + offset) % len(alphabet)
             encrypted_text += alphabet[new_index]
 
-caesar()
+caesar(text, shift)
