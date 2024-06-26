17ba9008e7357d2f486518abd291f6f67d5facdc
Mario Cooper <mariocooper00@hotmail.com>
2024-06-26 22:53:01 +0100

    use of for loop implemented

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
-index = alphabet.find(text[0].lower())
-shifted = alphabet[index + shift]
+for char in text:
+    index = alphabet.find(char)
+    print(char, index)
+    new_index = index + shift
