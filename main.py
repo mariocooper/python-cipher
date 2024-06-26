e901d4289f53a02fa2a482e6cf2fcb8f82a94fb5
Mario Cooper <mariocooper00@hotmail.com>
2024-06-26 23:09:49 +0100

    updated for loop to use '.lower()' method on 'text

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 encrypted_text = ''
 
-for char in text:
+for char in text.lower():
     index = alphabet.find(char)
     print(char, index)
     new_index = index + shift
