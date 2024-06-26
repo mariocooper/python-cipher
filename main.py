7ac9e5b6edf10f488e43ab6598dc4f3a27a51748
Mario Cooper <mariocooper00@hotmail.com>
2024-06-26 23:00:11 +0100

    'new_char' variable added to for loop

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
 for char in text:
     index = alphabet.find(char)
     print(char, index)
     new_index = index + shift
+    new_char = alphabet[new_index]
