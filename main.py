4cc688ceef8d9eaaa96ee619daa981a1a3d44ca8
Mario Cooper <mariocooper00@hotmail.com>
2024-06-26 23:04:03 +0100

    updated 'new_char' to 'encryped_text'

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 text = 'Hello World'
 shift = 3
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
+encrypted_text = ''
 
 for char in text:
     index = alphabet.find(char)
     print(char, index)
     new_index = index + shift
-    new_char = alphabet[new_index]
+    encrypted_text += alphabet[new_index]
