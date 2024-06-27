2c2b8c1f925c1d622d23fa2fc9ca96341a801e30
Mario Cooper <mariocooper00@hotmail.com>
2024-06-27 12:09:40 +0100

    updated logic to allow for non-letter characters, updated variable name

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-text = 'Hello Zaira'
+text = 'Hello Zaira!'
 custom_key = 'python'
 
 def vigenere(message, key, direction=1):
     key_index = 0
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    encrypted_text = ''
+    final_message = ''
 
     for char in message.lower():
-        if char == ' ':
-            encrypted_text += char
-        else:
+        if not char.isalpha():
+            final_message += char
+        else:        
             key_char = key[key_index % len(key)]
             key_index += 1
-            index = alphabet.find(char)
-            new_index = (index + offset * direction) % len(alphabet)
-            encrypted_text += alphabet[new_index]
+
+            # Define the offset and the encrypted/decrypted letter
             offset = alphabet.index(key_char)
-    return encrypted_text
+            index = alphabet.find(char)
+            new_index = (index + offset*direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    
+    return final_message
 
 encryption = vigenere(text, custom_key)
+print(encryption)
 decryption = vigenere(encryption, custom_key, -1)
